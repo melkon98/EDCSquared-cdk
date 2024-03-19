62ffabd1e34bc333a66e968cce469335b691af58
melkon98 <melkon.hovhannisian@gmail.com>
2024-03-19 12:39:25 +0400

    feat: add user wallet mutations

--- lib/user-wallets-stack.ts
+++ lib/user-wallets-stack.ts
@@ -17,37 +17,64 @@ export class UserWalletsStack extends Stack {
       Table.fromTableName(this, "userWalletsTable", USER_WALLETS_TABLE_NAME),
     );
 
-    userWalletsDS.createResolver("getUserWalletResolver", {
-      fieldName: "getUserWallet",
-      typeName: "Query",
-      requestMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.getUserWallet.req.vtl",
-      ),
-      responseMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.getUserWallet.res.vtl",
-      ),
-    });
+    try {
+      userWalletsDS.createResolver("getUserWalletResolver", {
+        fieldName: "getUserWallet",
+        typeName: "Query",
+        requestMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.getUserWallet.req.vtl",
+        ),
+        responseMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.getUserWallet.res.vtl",
+        ),
+      });
 
-    userWalletsDS.createResolver("listUserWalletsResolver", {
-      fieldName: "listUserWallets",
-      typeName: "Query",
-      requestMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.listUserWallets.req.vtl",
-      ),
-      responseMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.listUserWallets.res.vtl",
-      ),
-    });
+      userWalletsDS.createResolver("listUserWalletsResolver", {
+        fieldName: "listUserWallets",
+        typeName: "Query",
+        requestMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.listUserWallets.req.vtl",
+        ),
+        responseMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.listUserWallets.res.vtl",
+        ),
+      });
 
-    userWalletsDS.createResolver("userWalletsByOwnerResolver", {
-      fieldName: "userWalletsByOwner",
-      typeName: "Query",
-      requestMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.userWalletsByOwner.req.vtl",
-      ),
-      responseMappingTemplate: MappingTemplate.fromFile(
-        "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.userWalletsByOwner.res.vtl",
-      ),
-    });
+      userWalletsDS.createResolver("userWalletsByOwnerResolver", {
+        fieldName: "userWalletsByOwner",
+        typeName: "Query",
+        requestMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.userWalletsByOwner.req.vtl",
+        ),
+        responseMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Query.userWalletsByOwner.res.vtl",
+        ),
+      });
+
+      // Mutations:
+      userWalletsDS.createResolver("createUserWalletResolver", {
+        typeName: "Mutation",
+        fieldName: "createUserWallet",
+        requestMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Mutation.createUserWallet.req.vtl",
+        ),
+        responseMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Mutation.createUserWallet.res.vtl",
+        ),
+      });
+
+      userWalletsDS.createResolver("deleteUserWalletResolver", {
+        typeName: "Mutation",
+        fieldName: "deleteUserWallet",
+        requestMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Mutation.deleteUserWallet.req.vtl",
+        ),
+        responseMappingTemplate: MappingTemplate.fromFile(
+          "lib/amplify-export-edcsquared/api/edcsquared/amplify-appsync-files/resolvers/Mutation.deleteUserWallet.res.vtl",
+        ),
+      });
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
